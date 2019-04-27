cb1edd8b274496b86e21389dbb168a856e6984cc
Hellzbellz123 <smith.j.taylor2001@gmail.com>
2019-04-27 05:55:29 -0700

    Initial commit

---
+++ AddonMassUpdater/MainModule.cs
@@ -0,0 +1,66 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.IO;
+using System.Net;
+using System.Windows.Controls;
+
+public class LinkBuilder
+{
+    public static string downloadlink = string.Empty;
+
+    public void GetLinks()
+    {
+        var workingdirectory = Directory.GetCurrentDirectory();
+        int counter = 0;
+        string line;
+
+        // Read the file and display it line by line.
+        StreamReader file =
+            new StreamReader(workingdirectory + "/download/in.txt");
+        while ((line = file.ReadLine()) != null)
+        {
+            Console.WriteLine(line);
+            counter++;
+        }
+
+        file.Close();
+        Console.WriteLine("There were {0} lines.", counter);
+
+    }
+
+    public void Makeuseablelink()
+    {
+        string WorkingLink = @"https://www.curseforge.com/wow/addons/deadly-boss-mods" + "/download";
+        HtmlWeb web = new HtmlWeb();
+        var htmlDoc = web.Load(WorkingLink);
+        foreach (HtmlNode node in htmlDoc.DocumentNode.SelectNodes("//p/a"))
+        {
+            var hrefValue = node.Attributes["href"]?.Value;
+            downloadlink = "https://www.curseforge.com" + hrefValue;
+        }
+
+        Console.WriteLine("downloading");
+    }
+}
+
+public class DownloadFiles
+{
+    public void Downloader ()
+    {
+        using (WebClient wc = new WebClient())
+        {
+            wc.DownloadProgressChanged += ProgressBar_ValueChanged_1;
+            wc.DownloadFileAsync(
+                // Param1 = Link of file
+                new Uri("http://www.sayka.com/downloads/front_view.jpg"),
+                // Param2 = Path to save
+                "D:\\Images\\front_view.jpg"
+            );
+        }
+    }
+
+    void ProgressBar_ValueChanged_1(object sender, DownloadProgressChangedEventArgs e)
+    {
+        //ProgressBar.ValueChangedEvent = e.ProgressPercentage;
+    }
+}
---
+++ AddonMassUpdater/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace GetLinks
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BackupWTF_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void UpdateAddons_Click(object sender, RoutedEventArgs e)
+        {
+            var instance = new LinkBuilder();
+            instance.GetLinks();
+            instance.Makeuseablelink();
+            var instance2 = new DownloadFiles();
+            //instance2();
+        }
+
+        private void UpdaterFolder_Click(object sender, RoutedEventArgs e)
+        {
+            var workingdirectory = Directory.GetCurrentDirectory();
+            System.Diagnostics.Process.Start(workingdirectory);
+        }
+
+        private void WoWFolder_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void CleanCacheWTF_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void AddonList_TextBox(object sender, TextChangedEventArgs e)
+        {
+        }
+
+        private void ProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+        }
+
+        private void ConsoleControl_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void ProgressBar_ValueChanged_1(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+    }
+}
